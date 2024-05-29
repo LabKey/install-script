b7e1b8de9679d45741c67a5eb11cc0d3eadf2a8b
Stuart MacDonald <32204841+labkey-stuartm@users.noreply.github.com>
2024-05-29 15:59:51 -0700

    update chown permissions

--- install-labkey.bash
+++ install-labkey.bash
@@ -959,7 +959,7 @@ function step_configure_labkey() {
   chown -R "root:root" "${LABKEY_APP_HOME}/"
   chown -R "root:root" "${LABKEY_SRC_HOME}/"
   chown -R "$TOMCAT_USERNAME":"$TOMCAT_USERNAME" "${LABKEY_INSTALL_HOME}/"
-  chown -R "$TOMCAT_USERNAME"."$TOMCAT_USERNAME" "${TOMCAT_INSTALL_HOME}/"
+  chown -R "$TOMCAT_USERNAME":"$TOMCAT_USERNAME" "${TOMCAT_INSTALL_HOME}/"
   chown -R "$TOMCAT_USERNAME":"$TOMCAT_USERNAME" "${TOMCAT_TMP_DIR}/"
 
   # Configure for embedded
