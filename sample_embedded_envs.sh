bbb5a0cf6c1a90abc5e2c5a80b65ac6257310de6
Stuart MacDonald <32204841+labkey-stuartm@users.noreply.github.com>
2024-12-13 12:10:05 -0800

    update script defaults for v24.11.0

--- sample_embedded_envs.sh
+++ sample_embedded_envs.sh
@@ -12,7 +12,7 @@ export LABKEY_BASE_SERVER_URL="https://localhost"
 #export LABKEY_INSTALL_SKIP_START_LABKEY_STEP=1
 export POSTGRES_SVR_LOCAL="TRUE"
 
-export LABKEY_DIST_URL="https://lk-binaries.s3.us-west-2.amazonaws.com/downloads/release/community/23.11.2/LabKey23.11.2-3-community-embedded.tar.gz"
-export LABKEY_DIST_FILENAME="LabKey23.11.2-3-community-embedded.tar.gz"
-export LABKEY_VERSION="23.11.2"
+export LABKEY_DIST_URL="https://lk-binaries.s3.us-west-2.amazonaws.com/downloads/release/community/24.11.0/LabKey24.11.0-2-community.tar.gz"
+export LABKEY_DIST_FILENAME="LabKey24.11.0-2-community.tar.gz"
+export LABKEY_VERSION="24.11.0"
 export LABKEY_DISTRIBUTION="community"
